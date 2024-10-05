18f00f901bc94f5b898705b7480380d2c8c07973
santosh <santosh@DESKTOP-68V5I16>
2024-10-05 13:23:31 +0530

    Framework Created

---
+++ Framawork4/src/test/java/contact/ToCreateContactpage.java
@@ -0,0 +1,10 @@
+package contact;
+
+public class ToCreateContactpage {
+
+	public static void main(String[] args) {
+		System.out.println("caontact page created");
+
+	}
+
+}
---
+++ Framawork4/src/test/java/organazation/TocREATEORGANIZATIONPAGE.java
@@ -0,0 +1,10 @@
+package organazation;
+
+public class TocREATEORGANIZATIONPAGE {
+
+	public static void main(String[] args) {
+		System.out.println("Organization page created");
+
+	}
+
+}
